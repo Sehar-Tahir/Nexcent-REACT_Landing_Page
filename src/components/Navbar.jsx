49a5cf8d320fe7a2454ca3b13dd39c80c5d40cc8
Sehar-Tahir <sehartahirofficial@gmail.com>
2024-07-31 11:09:09 +0500

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,13 +4,13 @@ import { Link } from 'react-router-dom'
 const Navbar = () => {
   return (
     <>
-    <nav className="navbar navbar-expand-lg bg-light p-3">
+    <nav className="navbar navbar-expand-lg bg-light p-3 d-flex justify-content-between">
     <img src={Logo} alt="logo" className= "navbar-brand"  />
       <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
     
-      <div className="collapse navbar-collapse" id="navbarNav">
+      <div className="collapse navbar-collapse flex-grow-0" id="navbarNav">
         <ul className="navbar-nav mr-auto">
           <li className="nav-item">
               <Link className="nav-link" to="/">Home</Link>

676fbdd9b1708bedad2619b41483c43ef6d91611
Sehar-Tahir <sehartahirofficial@gmail.com>
2024-07-31 11:05:17 +0500

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,8 +6,6 @@ const Navbar = () => {
     <>
     <nav className="navbar navbar-expand-lg bg-light p-3">
     <img src={Logo} alt="logo" className= "navbar-brand"  />
-    {/* <div className='container-fluid'> */}
-      {/* <div className='d-flex align-items-center'></div> */}
       <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
